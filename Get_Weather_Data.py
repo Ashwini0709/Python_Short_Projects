a8bda4660833e9f9295e5474d8ce7339e2220c67
Ashwini Ramtekkar <ramtekkarashwini@gmail.com>
2025-04-18 16:42:02 +0530

    file updated with more code

--- Get_Weather_Data.py
+++ Get_Weather_Data.py
@@ -2,9 +2,34 @@
 
 import requests
 
-city = "Pune"  #Specify the city
+#Specify the city with longitude and latitude
+longitude = 73.8567   #Pune longitude
+latitude = 18.5204    #Pune latitude
 
-api_key = ""
+
+#api_key = does not require for this Open-Meteo Api. It is free.
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
+
+
+response = requests.get(url)
+data = response.json()
+
+
+#print(data)
+#by printing data, we get all weather data in JSON
+#if we want specific data then print each field
+#code below
+
+
+if response.status_code == 200:
+    current = data['current_weather']
+    print(f"Temperature: {current['temperature']}°C")
+    print(f"Windspeed: {current['windspeed']} km/h")
+    print(f"Weather code: {current['weathercode']}")
+else:
+    print("Error fetching weather data")
+
 
 
 
